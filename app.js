55783053ae4ed2d63be2cd31394421132782ee9a
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-02-24 22:11:29 -0600

    finishig up the app

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ mongoose.connect(process.env.MONGOOSE_KEY, { useNewUrlParser: true });
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId:String
+    googleId:String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMoongose)
@@ -96,16 +97,47 @@ app.get("/register", function (req, res) {
 })
 
 app.get("/secrets", function (req, res) {
+    User.find({"secret":{$ne:null}},function(err,foundUsers){
+        if(err){
+            console.log(err)
+        }else{
+            if(foundUsers){
+                res.render("secrets",{userWithSecrets:foundUsers})
+            }
+        }
+    })
+})
+
+app.get("/submit",function(req,res){
     if (req.isAuthenticated()) {
-        res.render("secrets")
+        res.render("submit")
     } else {
         res.redirect("/login")
     }
 })
 
+
 app.get("/logout", function (req, res) {
     req.logout()
     res.redirect("/")
+})
+
+app.post("/submit",function(req,res){
+    const submittedSecret=req.body.secret;
+    console.log(req.user.id)
+
+    User.findById(req.user.id,function(err,foundUser){
+        if(err){
+            console.log(err)
+        }else{
+            if(foundUser){
+                foundUser.secret=submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets")
+                })
+            }
+        }
+    })
 })
 
 app.post("/register", function (req, res) {
